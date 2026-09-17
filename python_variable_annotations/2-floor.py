20cc2eb2cafd9067412b28822c9c930e95939079
Ntwali111 <u.cynthia1@alustudent.com>
2026-09-17 22:04:56 +0000

    Add 2-floor.py and 2-main.py

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""Module for computing the floor of a float with type annotations."""
+import math
+
+
+def floor(n: float) -> int:
+    """Return the floor of a float as an integer."""
+    return math.floor(n)
